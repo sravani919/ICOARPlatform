e9d21290a198c112c1b7bb1b376c4352a996fd96
MO SHUJAA ALDEEN <mshujaa@node0006.palmetto.clemson.edu>
2023-05-17 16:07:05 -0400

    validation and preprocessing

--- data_preprocessing/__init__.py
+++ data_preprocessing/__init__.py
@@ -5,13 +5,19 @@ import preprocessor as p
 fmodel = fasttext.load_model("./models/lid.176.bin")
 
 
-def preprocess(filename, english_only, tweet_clean):
+def preprocess(filename, english_only, tweet_clean,drop_empty):
     df = pd.read_csv(filename)
     if english_only:
         m = df.apply(filter_non_english, axis=1)
         df = df[m]
     if tweet_clean:
         df["text"] = df["text"].apply(p.clean)
+    if drop_empty:
+        df.dropna(how='all', inplace=True)
+        df["text"] = df["text"].astype(str).str.lower()
+ 
+
+        
     df.to_csv(filename, index=False)
     return True
 
--- pages/5_Validation.py
+++ pages/5_Validation.py
@@ -1,8 +1,10 @@
 import glob
+from transformers import AutoConfig
 
 import pandas as pd
 import streamlit as st
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
+import nlu
 
 title = "Validation"
 
@@ -18,30 +20,94 @@ if "output" not in st.session_state:
 
 FILE = st.sidebar.selectbox("Select a file", [file for file in glob.glob("./data/*.csv")])
 
+
+MODELS = {
+    "cardiffnlp/twitter-roberta-base-sentiment": {
+        "tokenizer": AutoTokenizer,
+        "model": "cardiffnlp/twitter-roberta-base-sentiment"
+    },
+    "Toxic hugging face": {
+        "tokenizer": AutoTokenizer,
+        "model": "s-nlp/roberta_toxicity_classifier"
+    },
+    "Seethal/sentiment_analysis_generic_dataset": {
+        "tokenizer": AutoTokenizer,
+        "model": AutoModelForSequenceClassification
+    },
+    "Cyberbullying": {
+        "NLU": "",
+        "model": "en.classify.cyberbullying"
+    },
+    "Fakenews": {
+        "NLU": "",
+
+        "model": "en.classify.fakenews"
+    },
+    "Toxic": {
+        "NLU": "",
+        "model":"en.classify.toxic"
+    }
+}
+
 MODEL = st.sidebar.radio(
     "Select a model",
-    [
-        "cardiffnlp/twitter-roberta-base-sentiment",
-        "finiteautomata/bertweet-base-sentiment-analysis",
-        "Seethal/sentiment_analysis_generic_dataset",
-    ],
+    list(MODELS.keys())
 )
 
 
-def predict(text):
-    tokenizer = AutoTokenizer.from_pretrained(MODEL)
-    model = AutoModelForSequenceClassification.from_pretrained(MODEL)
-    inputs = tokenizer(text, return_tensors="pt")
-    outputs = model(**inputs)
-    output = outputs.logits.argmax().item()
-    # Labels: 0 -> Negative; 1 -> Neutral; 2 -> Positive
-    label = "Negative" if output == 0 else "Neutral" if output == 1 else "Positive"
-    return label
+def load_model(model_info):
+    if "tokenizer" in model_info:
+        pass
+
+    else:
+        loaded_model = nlu.load(model_info["model"])
+        return loaded_model
+
+
+def predict(text, model_info,model):
+    if "tokenizer" in model_info:
+        # tokenizer, model = model_info["tokenizer"], model_info["model"]
+        tokenizer = AutoTokenizer.from_pretrained(model_info["model"])
+        model = AutoModelForSequenceClassification.from_pretrained(model_info["model"])
+        config = AutoConfig.from_pretrained(model_info["model"])
+
+        inputs = tokenizer(text, return_tensors="pt")
+        outputs = model(**inputs)
+        output = outputs.logits.argmax().item()
+        label = config.id2label[output]
+
+        return label
+    else:
+
+        result = model.predict(text)
+        result = result.drop("sentence_embedding_use", axis=1)
+        # result = result.drop("sentence", axis=1)
+        # column_names = result.columns.tolist()
+
+    return result
 
 
 if st.sidebar.button("Predict"):
     df = pd.read_csv(FILE)
+    
+    model_info = MODELS[MODEL]
+    
+    model = load_model(model_info)
+
     for index, row in df.iterrows():
-        df.loc[index, "sentiment"] = predict(row["text"])
+        if "tokenizer" in model_info:
+            df.loc[index, "sentiment"] = predict(row["text"],model_info,[])
+
+        else:
+
+            result = predict(row["text"], [] ,model)
+
+            df.loc[index, result.columns ] = result.values[0]
+
+       
+
+
+
         with placeholder.container():
             st.write(df)
+
