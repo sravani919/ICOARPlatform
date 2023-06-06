ef7849d83c370c5fdb089cc283b35b4d6987596e
moaldeen <mshujaa@clemson.edu>
2023-06-06 18:35:03 -0400

    delete the data_analysis

--- pages/6_Visualisation.py
+++ pages/6_Visualisation.py
@@ -1,8 +1,12 @@
 import glob
 
+import gensim
 import matplotlib.pyplot as plt
 import pandas as pd
+import pyLDAvis
+import pyLDAvis.gensim_models as gensimvis
 import streamlit as st
+from nltk.tokenize import word_tokenize
 
 if "filename_pred" not in st.session_state:
     st.session_state.filename_pred = ""
@@ -14,7 +18,7 @@ if st.button("Load"):
 
 if st.session_state.filename_pred != "":
     df = pd.read_csv(st.session_state.filename_pred)
-    options = ["Bar Plot", "Pie Chart"]
+    options = ["Topic Modeling", "Bar Plot", "Pie Chart"]
     selected_option = st.selectbox("Select an type of visualisation", options)
     data = df
     fig, ax = plt.subplots()
@@ -35,3 +39,33 @@ if st.session_state.filename_pred != "":
         ax.pie(value_counts.values, labels=data["sentiment"].unique(), autopct="%1.1f%%")
         ax.set_title("Pie Chart")
         st.pyplot(fig)
+
+    elif selected_option == "Topic Modeling":
+        data = data["text"].tolist()
+
+        # Tokenize the documents
+        tokenized_data = [word_tokenize(text) for text in data]
+
+        # Create dictionary and corpus
+        dictionary = gensim.corpora.Dictionary(tokenized_data)
+        corpus = [dictionary.doc2bow(text) for text in tokenized_data]
+
+        # Train LDA model
+        lda_model = gensim.models.LdaModel(corpus=corpus, id2word=dictionary, num_topics=10, passes=10)
+
+        # Extract topic labels
+        topic_labels = []
+        topics = lda_model.show_topics(num_topics=10, num_words=10, formatted=False)
+        for topic in topics:
+            words = [word for word, _ in topic[1]]
+            topic_labels.append(" ".join(words))
+
+        # Display topic labels
+        for i, label in enumerate(topic_labels):
+            st.write(f"Topic {i + 1}: {label}")
+
+        # Prepare data for visualization
+        vis_data = gensimvis.prepare(lda_model, corpus, dictionary)
+        html_string = pyLDAvis.prepared_data_to_html(vis_data)
+
+        st.components.v1.html(html_string, width=1000, height=600, scrolling=True)
