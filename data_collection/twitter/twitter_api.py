da9693907f11c2a73d932b2309e474767d5fff9f
EthanDesktop <telan4892@gmail.com>
2023-07-07 01:45:42 -0400

    Twitter scraping with accounts

--- data_collection/twitter/twitter_api.py
+++ data_collection/twitter/twitter_api.py
@@ -1,11 +1,7 @@
-import os
 import time  # For sleeping between requests
 
-import pandas as pd
 import streamlit as st
 import tweepy
-
-from .utils import download_image
 
 
 def init_connection():
@@ -285,55 +281,3 @@ def grab_tweets(keywords, count: int, must_have_images, start_time, end_time):
     tweets = format_tweet_results(res)
 
     return tweets
-
-
-def save_tweet_images(tweet, images_path):
-    """
-    Takes in a single tweet dictionary and then downloads the images off the internet
-    and saves them to the given file path
-    :param tweet: The tweet dictionary
-    :param images_path: The file path to save the images to
-
-    """
-    id = tweet["id"]
-    tweet_folder = images_path + "/" + str(id)  # Folder for all the images from this tweet
-    # Creating a folder for the images if it doesn't already exist
-    if not os.path.exists(tweet_folder):
-        os.makedirs(tweet_folder)
-
-    # Checking if the tweet has any images
-    urls = tweet["image_urls"]
-    if urls is None or len(urls) == 0:
-        return []
-
-    # Downloading and saving the images
-    for i in range(len(urls)):
-        image = download_image(urls[i])
-        if image is not None:
-            image.save(f"{tweet_folder}/{i}.jpg")
-
-
-def save_tweets(tweets, filename):
-    df = pd.DataFrame(tweets)
-    file_path = f"data/{filename}.csv"
-    df.to_csv(file_path, index=False)
-
-    return file_path
-
-
-def save_images(tweets, filename, i):
-    """
-    Saves the images from the given tweet to the given file path
-    Uses an index i so a loading bar can be implemented on the front end while still having all the code here
-    :param tweets: The full list of tweet dictionaries
-    :param filename: The filename being used to save the tweets
-    :param i: The index of the tweet to save the images from (allows for easy iteration for a loading bar)
-    :return: The file path to the folder containing the images from the tweet
-    """
-    images_path = f"data/{filename}_images"
-    if i == 0:
-        if not os.path.exists(images_path):
-            os.makedirs(images_path)
-    save_tweet_images(tweets[i], images_path)
-
-    return images_path
---
+++ data_collection/twitter/twitter_scraper.py
@@ -0,0 +1,99 @@
+import asyncio
+
+import streamlit as st
+from twscrape import API, AccountsPool, gather
+
+
+def get_accounts():
+    # Reading from accounts.csv where the info is seperated by colons, not commas
+    f = st.secrets.twitter.accounts
+
+    accounts = []
+    for account in f.split(","):
+        values = [v.strip() for v in account.split(":")]
+        accounts.append(values)
+
+    return accounts
+
+
+def format_tweets(tweets):
+    formatted_tweets = []
+    for tweet in tweets:
+        mentions = []
+        for mention in tweet.mentionedUsers:
+            mentions.append(mention.username)
+
+        coordinates = None
+        if tweet.coordinates is not None:
+            coordinates = [tweet.coordinates.longitude, tweet.coordinates.latitude]
+
+        links = []
+        for link in tweet.links:
+            links.append(link.url)
+
+        image_urls = []
+        for photo in tweet.media.photos:
+            image_urls.append(photo.url)
+        # for video in tweet.media.videos:
+        #     video_urls.append(video.thumbnailUrl)
+
+        links = []
+        for link in tweet.links:
+            links.append(link.url)
+
+        formatted_tweet = {
+            "id": tweet.id,
+            "text": tweet.rawContent,
+            "date": tweet.date,
+            "author_id": tweet.user.id,
+            "author_name": tweet.user.username,
+            "retweets": tweet.retweetCount,
+            "lang": tweet.lang,
+            "mentions": mentions,
+            "hashtags": tweet.hashtags,
+            "coordinates": coordinates,
+            "place": tweet.place,
+            "url": tweet.url,
+            "image_urls": image_urls,
+            "links": links,
+        }
+        formatted_tweets.append(formatted_tweet)
+    return formatted_tweets
+
+
+def grab_tweets(keywords, tweet_count, must_have_images, start, end):
+    return asyncio.run(a_grab_tweets(keywords, tweet_count, must_have_images, start, end))
+
+
+async def a_grab_tweets(keywords, tweet_count, must_have_images, start, end):
+    status_text = st.empty()
+
+    # Setting up the accounts pool
+    pool = AccountsPool()
+    for account in get_accounts():
+        await pool.add_account(*account)
+
+    # Logging in to all new accounts
+    status_text.text("Logging in to all new accounts...")
+    await pool.login_all()
+
+    # Creating an API object
+    api = API(pool)
+
+    # Search API (latest tab)
+    query = keywords[:]
+
+    if start is not None:
+        query += f" since:{start}"
+    if end is not None:
+        query += f" until:{end}"
+    if must_have_images:
+        query += " filter:images"
+
+    status_text.text("Searching for tweets...")
+    generator = api.search(query, limit=tweet_count)
+    tweets = await gather(generator)
+    status_text.text("Done searching for tweets!")
+
+    formatted_tweets = format_tweets(tweets)
+    return formatted_tweets
