5e53fa42f1cbbd0a7435c58f400cd4d8a7a353a8
moaldeen <mshujaa@clemson.edu>
2023-07-12 19:00:29 -0400

    add GPT annotaion and temproal analysis 3

--- pages/6_Visualisation.py
+++ pages/6_Visualisation.py
@@ -46,28 +46,22 @@ if st.session_state.filename_pred != "":
     elif selected_option == "Topic Modeling":
         data = data["text"].tolist()
 
-        # Tokenize the documents
         tokenized_data = [word_tokenize(text) for text in data]
 
-        # Create dictionary and corpus
         dictionary = gensim.corpora.Dictionary(tokenized_data)
         corpus = [dictionary.doc2bow(text) for text in tokenized_data]
 
-        # Train LDA model
         lda_model = gensim.models.LdaModel(corpus=corpus, id2word=dictionary, num_topics=10, passes=10)
 
-        # Extract topic labels
         topic_labels = []
         topics = lda_model.show_topics(num_topics=10, num_words=10, formatted=False)
         for topic in topics:
             words = [word for word, _ in topic[1]]
             topic_labels.append(" ".join(words))
 
-        # Display topic labels
         for i, label in enumerate(topic_labels):
             st.write(f"Topic {i + 1}: {label}")
 
-        # Prepare data for visualization
         vis_data = gensimvis.prepare(lda_model, corpus, dictionary)
         html_string = pyLDAvis.prepared_data_to_html(vis_data)
 
@@ -85,13 +79,10 @@ if st.session_state.filename_pred != "":
             .split(",")
         )
 
-        # keywords = input("Enter the keywords of interest seperated by comma (i.e., covid, lockdown, ... ): ")
-
         masks = [data["text"].str.contains(keyword.strip(), case=False, na=False) for keyword in keywords]
 
         daily_counts = [data.loc[mask, "date"].value_counts().sort_index().resample("D").sum() for mask in masks]
 
-        # Create the layout for the first figure (daily counts)
         layout_counts = go.Layout(
             title="Daily Text Pattern Counts",
             xaxis=dict(title="Date"),
@@ -99,61 +90,41 @@ if st.session_state.filename_pred != "":
             legend=dict(orientation="h"),
         )
 
-        # Create the figure for the first figure (daily counts)
         fig_counts = go.Figure(layout=layout_counts)
 
-        # Add traces to the first figure
         for i, count in enumerate(daily_counts):
             fig_counts.add_trace(go.Scatter(x=count.index, y=count.values, mode="lines", name=keywords[i].capitalize()))
 
-        # Display the first figure (daily counts)
-        # fig_counts.show()
         st.plotly_chart(fig_counts)
 
-        # Create the layout for the second figure (top posters)
         layout_top_posters = go.Layout(
             title="Top Posters for Each Keyword", xaxis=dict(title="User"), yaxis=dict(title="Count"), barmode="group"
         )
 
-        # Create the figure for the second figure (top posters)
         fig_top_posters = go.Figure(layout=layout_top_posters)
 
-        # Add traces to the second figure
         for i, mask in enumerate(masks):
             top_posters = data.loc[mask, "user_name"].value_counts().nlargest(10)
             fig_top_posters.add_trace(go.Bar(x=top_posters.index, y=top_posters.values, name=keywords[i].capitalize()))
 
-        # Display the second figure (top posters)
-        # fig_top_posters.show()
-        # fig.tight_layout()
-
-        # Display the plot
         st.plotly_chart(fig_top_posters)
 
         trace1 = go.Scatter(x=daily_counts.index, y=daily_counts.values, mode="lines", name="Daily Tweet Count")
 
-        # Create the layout for daily tweet counts
         layout1 = go.Layout(title="Daily Tweet Counts", xaxis=dict(title="Date"), yaxis=dict(title="Count"))
 
-        # Create the figure for daily tweet counts
         fig1 = go.Figure(data=[trace1], layout=layout1)
 
-        # Display the figure for daily tweet counts
         st.plotly_chart(fig1)
 
-        # Calculate the top 10 posters
         top_posters = data["user_name"].value_counts().nlargest(10)
 
-        # Create a bar plot for the top 10 posters
         trace2 = go.Bar(x=top_posters.index, y=top_posters.values, name="Top 10 Posters")
 
-        # Create the layout for top posters
         layout2 = go.Layout(title="Top 10 Posters", xaxis=dict(title="User"), yaxis=dict(title="Tweet Count"))
 
-        # Create the figure for top posters
         fig2 = go.Figure(data=[trace2], layout=layout2)
 
-        # Display the figure for top posters
         st.plotly_chart(fig2)
 
     elif selected_option == "User Network":
