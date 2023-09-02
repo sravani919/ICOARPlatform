932da305fc8c55c60e01d625360272a0e3e890fc
Pranav <prsilimkhan@gmail.com>
2023-09-02 09:30:39 -0400

    New ui changes to the icoar project

--- Home.py
+++ Home.py
@@ -1,5 +1,41 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="ICOAR",
-)
+from tabs.image.meme_classification import meme_classification
+
+st.set_page_config(layout="wide")
+
+
+def read_css_file():
+    with open("style.css") as f:
+        return f.read()
+
+
+st.markdown(f"<style>{read_css_file()}</style>", unsafe_allow_html=True)
+
+tab1, tab2 = st.tabs(["Text Classification", "Image Classification"])
+
+with tab1:
+    st.subheader("Text classification")
+    tab1_1, tab1_2, tab1_3 = st.tabs(["Retireval", "Preprocessing", "Classification"])
+    with tab1_1:
+        st.subheader("Work in progress")
+
+with tab2:
+    st.subheader("Image classification")
+    tab2_1, tab2_2, tab2_3, tab2_4 = st.tabs(
+        ["Retrieval", "Classification", "Meme Classification", "Deepfake Detection"]
+    )
+    with tab2_1:
+        st.subheader("Work in progress")
+    with tab2_3:
+        st.markdown(":bulb: In this tab, you can classify memes as ***Hateful*** or ***Non-Hateful***.")
+
+        multi = """:bulb: Steps -
+        :one:  Upload a zip file containing memes (jpeg, png, jpg) format.
+        :two:  We will extract the text from the memes for you.
+        :three:  Make sure it is matching to the actual text in the meme.
+        :four:  Click on the predict button.
+            """
+        st.markdown(multi)
+
+        meme_classification()
--- pages/4_Training.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-title = "Training"
-
-st.set_page_config(page_title=title)
-
-st.sidebar.header(title)
