684c0e85f05bed4de377d0e5f3c45cb64ce23e40
pranavsilimkhan <pranavsilimkhan97@gmail.com>
2023-09-10 17:23:42 -0400

    Added df related files

--- tabs/image/deepfake_detection.py
+++ tabs/image/deepfake_detection.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import numpy as np
+from model.df.classifiers import *
+from model.df.pipeline import *
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 def df_detection():
-    st.write("df detection")
+    st.write('df')
