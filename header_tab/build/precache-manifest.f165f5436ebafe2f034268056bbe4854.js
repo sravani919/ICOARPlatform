6e6e66dfb68d8becbfda6ea55440930276433e41
Sravani Pati <spati@MacBookPro.vyvebroadband.net>
2025-12-04 03:34:42 -0500

    Include header_tab build for Streamlit component

---
+++ header_tab/build/precache-manifest.f165f5436ebafe2f034268056bbe4854.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25c916071bda72459fcf293b3b190051",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ccde9a00324b57c36cf4",
+    "url": "./static/js/2.931ac2ed.chunk.js"
+  },
+  {
+    "revision": "73df2c9ae8def1ba500c58c619a9cfef",
+    "url": "./static/js/2.931ac2ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d11f94191dc03d31f5c",
+    "url": "./static/js/main.8997c892.chunk.js"
+  },
+  {
+    "revision": "c639cdbcf0da2a2e6302",
+    "url": "./static/js/runtime-main.97b4e619.js"
+  },
+  {
+    "revision": "228da65b1c711457758e48a2e592819e",
+    "url": "./static/media/icoar_logo.228da65b.png"
+  }
+]);
