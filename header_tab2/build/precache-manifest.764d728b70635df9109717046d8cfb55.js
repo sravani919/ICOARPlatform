0cb6f2cd3e2f39e9089d73642197538302928846
Sravani Pati <spati@MacBookPro.vyvebroadband.net>
2025-12-08 14:42:32 -0500

    Fix gitignore and keep React build folders for Streamlit

---
+++ header_tab2/build/precache-manifest.764d728b70635df9109717046d8cfb55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d96076173a78e0f3599006e1a57f22f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61be26dd75d37e3e47a6",
+    "url": "./static/js/2.11cace18.chunk.js"
+  },
+  {
+    "revision": "73df2c9ae8def1ba500c58c619a9cfef",
+    "url": "./static/js/2.11cace18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e2dc29690d58632c652",
+    "url": "./static/js/main.89be6324.chunk.js"
+  },
+  {
+    "revision": "c639cdbcf0da2a2e6302",
+    "url": "./static/js/runtime-main.97b4e619.js"
+  }
+]);
